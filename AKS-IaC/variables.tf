2a2840e4eee0b0e95fa5a45c3d585a53b6dcd03c
prasancto <prasancto@gmail.com>
2020-09-10 11:22:43 +0530

    update with variable in commandline

--- AKS-IaC/variables.tf
+++ AKS-IaC/variables.tf
@@ -1,16 +1,26 @@
-variable "location" {}
+variable "serviceprinciple_id" {
+}
 
-variable "client_id" {}
+variable "serviceprinciple_key" {
+}
 
-variable "client_secret" {}
+variable "tenant_id" {
+}
+
+variable "subscription_id" {
+}
+
+variable "ssh_key" {
+}
+
+variable "location" {
+}
+
+variable "kubernetes_version" {
+    default = "1.16.13"
+}
 
 variable "admin_username" {
     type = string
     description = "Administrator user name for virtual machine"
-}
-
-variable "ssh_key" {}
-
-variable "kubernetes_version" {
-    default = "1.16.13"
 }
